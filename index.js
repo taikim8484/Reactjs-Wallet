6e9b0842125d1ec7d145a78490a6052e970f56aa
Bill <baotuan0.0@gmail.com>
2017-11-28 18:53:01 +0700

    Config get "/" request for deploy

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.use(bodyParser.json());
 require("./routes/authRoutes")(app);
 require("./routes/transferRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.NODE_ENV || 8888;
 
 app.listen(PORT);
